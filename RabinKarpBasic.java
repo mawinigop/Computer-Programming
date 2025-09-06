e1dba09773cb4d3257903d9a8b8b962af973185e
Marwin Mandocdoc <50324918+mawinigop@users.noreply.github.com>
2025-09-07 01:25:05 +0800

    Update RabinKarpBasic.java

--- RabinKarpBasic.java
+++ RabinKarpBasic.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
-// quiz
-public class RabinKarpBasic {
+public class RabinKarp {
     public static void search(String text, String pattern) {
         int m = pattern.length();
-        int n = text.length();                                    
+        int n = text.length();
+        boolean found = false;                                    
 
         for (int i = 0; i <= n - m; i++) {
             String sub = text.substring(i, i + m);
             if (sub.equalsIgnoreCase(pattern)) {
                 System.out.println("Pattern found at index: " + i);
-            }   
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Pattern not found");
         }
     }
-
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
